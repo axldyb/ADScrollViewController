726c446e1b1ecbb3fb7dc4797417ff5e9bff810d
Aksel Dybdal <akseldybdal@gmail.com>
2012-10-15 21:07:10 +0100

    Initial Commit

---
+++ ADScrollViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ADScrollViewController
+//
+//  Created by Aksel Dybdal on 15.10.12.
+//  Copyright (c) 2012 Aksel Dybdal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ADScrollViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ADScrollViewController
+//
+//  Created by Aksel Dybdal on 15.10.12.
+//  Copyright (c) 2012 Aksel Dybdal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
